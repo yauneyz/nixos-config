58a5bd88e81acf90b382d8f4844805bd38ac2caa
Zac Yauney <zac.yauney@gmail.com>
2026-07-05 22:01:32 -0700

    Update snorlax config

--- pkgs/snorlax/release.nix
+++ pkgs/snorlax/release.nix
@@ -1,7 +1,7 @@
 {
   version = "0.1.0";
   storePath = builtins.fetchurl {
-    url = "file:///nix/store/01aiyn7z8in0y9g5wfskk40is9cbhj71-snorlax.AppImage";
-    sha256 = "0w332i94c5pxhndg73s3mk6cdgf200lv6mczksw70j59dprbvm4p";
+    url = "file:///nix/store/4ibwxqfyckyb6kjf9iv9al6zw2fi7ssl-snorlax.AppImage";
+    sha256 = "0l3cmkqqqpqywicwb86qap0qdns04fqn2d95irjrwgnjsmyxgq23";
   };
 }
